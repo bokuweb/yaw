73b83d5c87cc8f576a2121bdc8f3b3fa83b05bdc
rhysd <lin90162@yahoo.co.jp>
2020-01-27 01:39:37 +0900

    use AsRef<[u8]> instead of &[u8]

--- src/lib.rs
+++ src/lib.rs
@@ -18,12 +18,12 @@ pub use vm::{
     MemoryRef, RuntimeError, TableInstance, TableRef, VM,
 };
 
-pub fn instantiate<'a>(
-    buf: &[u8],
+pub fn instantiate<'a, B: AsRef<[u8]>>(
+    buf: B,
     imports: Option<&'a dyn ImportResolver>,
 ) -> Result<VM<'a>, error::YawError> {
     let mut magic_number = [0; 4];
-    let mut reader = &buf[..];
+    let mut reader = buf.as_ref();
     reader.read_exact(&mut magic_number)?;
     let magic_number = String::from_utf8(magic_number.to_vec())?;
     if magic_number != "\0asm" {
